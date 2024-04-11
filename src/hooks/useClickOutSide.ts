6755b9dff53bb017f17d3091f37d0d097c8e4073
hieplb071191 <lehiep071191>
2024-04-11 20:38:12 +0700

    Merge branch 'main' of github.com:hieplb071191/test-deploy-frontend

--- src/hooks/useClickOutSide.ts
+++ src/hooks/useClickOutSide.ts
@@ -3,10 +3,10 @@ import useEventListener from "./useEventListener"
 
 
 export default function useClickOutside(ref: any, cb: Function) {
-    if (window && typeof window !== undefined) {
+
         useEventListener("click",( e: Event) => {
             if (ref.current == null || ref.current.contains(e.target)) return
             cb(e)
         }, window)
-    }
+    
 }
